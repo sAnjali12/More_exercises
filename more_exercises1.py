694e2ac027eb62aba7dda0b531337d6777fc423e
sAnjali12 <anjalisen060@gmail.com>
2019-06-15 20:43:03 +0530

    first commit

---
+++ more_exercises1.py
@@ -0,0 +1,10 @@
+number = 1
+while number<=1000:
+	if number%21==0:
+		print "navgurukul",number
+	elif number%3==0:
+		print "nav",number
+	elif number%7==0:
+		print "gurukul",number
+	number = number+1
+
